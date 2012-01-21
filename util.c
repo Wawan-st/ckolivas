2aef37fb0d35c3bef4354f6f47644e49990bf315
Midnight Magic <github@quake.ca>
2012-01-21 15:49:25 -0800

    Re-add TCP_NODELAY as a user-controllable option to the CURL networking section, primarily for local gbE networks which really have no problem at all with lots of tiny little packets storming around.

--- util.c
+++ util.c
@@ -336,7 +336,9 @@ json_t *json_rpc_call(CURL *curl, const char *url,
 	curl_easy_setopt(curl, CURLOPT_URL, url);
 	curl_easy_setopt(curl, CURLOPT_ENCODING, "");
 	curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1);
-	curl_easy_setopt(curl, CURLOPT_TCP_NODELAY, 1);
+	if (opt_want_tcp_nodelay) {
+		curl_easy_setopt(curl, CURLOPT_TCP_NODELAY, 1);
+	}
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, all_data_cb);
 	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &all_data);
 	curl_easy_setopt(curl, CURLOPT_READFUNCTION, upload_data_cb);
@@ -737,7 +739,9 @@ bool get_dondata(char **url, char **userpass)
 	curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
 	curl_easy_setopt(curl, CURLOPT_ENCODING, "");
 	curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1);
-	curl_easy_setopt(curl, CURLOPT_TCP_NODELAY, 1);
+	if (opt_want_tcp_nodelay) {
+		curl_easy_setopt(curl, CURLOPT_TCP_NODELAY, 1);
+	}
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, all_data_cb);
 	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &all_data);
 	curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, curl_err_str);
