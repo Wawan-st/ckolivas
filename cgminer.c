83ddf4abc94e2ab1f4f07f3253d29adaafde655a
DmaxL <dmaxl@users.noreply.github.com>
2014-07-12 16:35:39 -0700

    Fix typo in getwork share submission

--- cgminer.c
+++ cgminer.c
@@ -3187,7 +3187,7 @@ static bool submit_upstream_work(struct work *work, CURL *curl, bool resubmit)
 		endian_flip128(work->data, work->data);
 
 		/* build hex string */
-		hexstr = bin2hex(work->data, 118);
+		hexstr = bin2hex(work->data, 128);
 		s = strdup("{\"method\": \"getwork\", \"params\": [ \"");
 		s = realloc_strcat(s, hexstr);
 		s = realloc_strcat(s, "\" ], \"id\":1}");
